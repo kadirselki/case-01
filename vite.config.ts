ebba2616c6fc35c916f7f0d3c1011f733719f6a9
Kadir Selki <kadir.selki@viravira.co>
2023-10-24 13:17:21 +0300

    updated project settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default ({ mode }) => {
-    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
 
     return defineConfig({
         plugins: [react()],
         server: {
             port: parseInt(process.env.PORT),
             open: true,
-            host: "0.0.0.0",
+            host: '0.0.0.0',
         },
         resolve: {
             alias: {
