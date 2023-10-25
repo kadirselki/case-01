0698f710619b9dfc2cfa838f39d691fddc2975bb
Kadir Selki <kadir.selki@viravira.co>
2023-10-25 11:49:56 +0300

    api initialized with docker

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { App } from './app';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { StrictMode } from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './utils/apollo-instance';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <ApolloProvider client={client}>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </ApolloProvider>
     </StrictMode>,
 );
